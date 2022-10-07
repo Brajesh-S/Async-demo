94d8079c025cbf53102dc51da4be74dd510cac51
brajesh-s <brajeshsekar@outlook.com>
2022-10-07 05:09:31 -0700

    callback function-async

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('Before');
+getUser(1, function(user) {
+    console.log('User', user);
+})
+console.log('After');
+
+function getUser(id, callback ) {
+    setTimeout(() => {
+        console.log('Reading a user from db..');
+        callback({ id: id, gitHubUsername: 'biju'});
+}, 2000);
+
+}
